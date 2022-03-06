a3c1d94037088b18d0dbd3b0106f4a5ca5889d2c
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-03-06 14:00:00 +0200

    feat: Spring boot restful APIs

---
+++ restfulwebservices/src/main/java/billbenon/me/restfulwebservices/jwt/resource/AuthenticationException.java
@@ -0,0 +1,7 @@
+package billbenon.me.restfulwebservices.jwt.resource;
+
+public class AuthenticationException extends RuntimeException {
+	public AuthenticationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
---
+++ restfulwebservices/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo(id, username,description,target_date,is_done)
+values(10001, 'billbenon', 'Learn JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10002, 'billbenon', 'Learn Data JPA', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'billbenon', 'Learn Microservices', sysdate(), false);
+
+insert into todo(id, username,description,target_date,is_done)
+values(10003, 'billbenon', 'Learn Spring boot swagger UI Documentation', sysdate(), false);
