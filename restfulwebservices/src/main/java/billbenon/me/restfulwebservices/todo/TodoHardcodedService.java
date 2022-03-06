8f8d4ab145e64b6dbc7eedcb224cb655ba8082f3
BillBenon <61540696+BillBenon@users.noreply.github.com>
2022-03-06 14:00:00 +0200

    fit: rest APIs finalization

--- restfulwebservices/src/main/java/billbenon/me/restfulwebservices/todo/TodoHardcodedService.java
+++ restfulwebservices/src/main/java/billbenon/me/restfulwebservices/todo/TodoHardcodedService.java
@@ -14,9 +14,9 @@ public class TodoHardcodedService {
 	private static long idCounter = 0;
 	
 	static {
-		todos.add(new Todo(++idCounter, "in28minutes","Learn to Dance 2", new Date(), false ));
-		todos.add(new Todo(++idCounter, "in28minutes","Learn about Microservices 2", new Date(), false ));
-		todos.add(new Todo(++idCounter, "in28minutes","Learn about Angular", new Date(), false ));
+		todos.add(new Todo(++idCounter, "billbenon","Learn to Dance 2", new Date(), false ));
+		todos.add(new Todo(++idCounter, "billbenon","Learn about Microservices 2", new Date(), false ));
+		todos.add(new Todo(++idCounter, "billbenon","Learn about Angular", new Date(), false ));
 	}
 	
 	public List<Todo> findAll() {
